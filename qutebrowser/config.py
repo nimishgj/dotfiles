c3550a6aa2dfed6814686996dcc98600206f529c
Nimish G J <84202076+nimishgj@users.noreply.github.com>
2025-05-15 03:02:42 +0530

    Update config.py

--- qutebrowser/config.py
+++ qutebrowser/config.py
@@ -1,2 +1,5 @@
 config.bind('<Ctrl-j>', 'completion-item-focus --history next', mode='command')
 config.bind('<Ctrl-k>', 'completion-item-focus --history prev', mode='command')
+config.bind('<Ctrl-t>', 'open -t')
+
+config.load_autoconfig()
