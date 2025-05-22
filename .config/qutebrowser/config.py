6c972fe593fdac51d6c5a8cfac38408c7bc3054f
nimishgj <nimishgj444@gmail.com>
2025-05-23 00:54:13 +0530

    restructure using stow

--- .config/qutebrowser/config.py
+++
@@ -1,2 +0,0 @@
-config.bind('<Ctrl-j>', 'completion-item-focus --history next', mode='command')
-config.bind('<Ctrl-k>', 'completion-item-focus --history prev', mode='command')
---
+++ stow.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+mkdir -v -p ~/.config
+mkdir -v -p ~/bin
+
+stow --adopt -v -R -t  ~/.config/ tmux
+stow --adopt -v -R -t  ~/.config/ ghostty
+stow --adopt -v -R -t  ~/.config/ nvim
+stow --adopt -v -R -t  ~ vim
+stow --adopt -v -R -t  ~ git
+stow --adopt -v -R -t  ~/bin bin
+stow --adopt -v -R -t  ~/.config tmi
+stow --adopt -v -R -t  ~ qutebrowser
+stow --adopt -v -R -t  ~ asdf
+stow --adopt -v -R -t  ~ brew
+stow --adopt -v -R -t  ~ zsh
