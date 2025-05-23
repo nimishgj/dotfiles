404f2e2e066ade2d94fe9ef0fc718d0cb0def907
nimishgj <nimishgj444@gmail.com>
2025-05-23 11:01:27 +0530

    update defaults to kill dock

--- defaults/run.sh
+++ defaults/run.sh
@@ -1,2 +1,8 @@
 defaults write com.apple.finder QuitMenuItem -bool true
 killall Finder
+defaults write com.apple.dock autohide -bool true
+defaults write com.apple.dock autohide-delay -float 0
+defaults write com.apple.dock autohide-time-modifier -float 0
+defaults write com.apple.dock showhidden -bool true
+killall Dock
+
