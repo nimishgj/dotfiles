193d2547d570ef3ad477cc2fd84a6ed0222d42bb
nimishgj <nimishgj444@gmail.com>
2025-05-26 13:09:43 +0530

    add qutebroser config bindings

--- qutebrowser/.qutebrowser/config.py
+++ qutebrowser/.qutebrowser/config.py
@@ -1,5 +1,6 @@
 config.bind('<Ctrl-j>', 'completion-item-focus --history next', mode='command')
 config.bind('<Ctrl-k>', 'completion-item-focus --history prev', mode='command')
 config.bind('<Ctrl-t>', 'open -t')
+config.bind('jj', 'mode-leave', mode='insert')
 
 config.load_autoconfig()
