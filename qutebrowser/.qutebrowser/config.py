306d454069c3ec7c8e8b9fa14ae85ced84d2bad8
nimishgj <nimishgj444@gmail.com>
2025-06-06 01:26:17 +0530

    qutebrowser config update

--- qutebrowser/.qutebrowser/config.py
+++ qutebrowser/.qutebrowser/config.py
@@ -2,4 +2,7 @@ config.bind('<Ctrl-j>', 'completion-item-focus --history next', mode='command')
 config.bind('<Ctrl-k>', 'completion-item-focus --history prev', mode='command')
 config.bind('<Ctrl-t>', 'open -t')
 
+c.fonts.tabs.unselected = '20pt JetbrainsMono'
+c.fonts.prompts = '20pt JetbrainsMono'
+
 config.load_autoconfig()
